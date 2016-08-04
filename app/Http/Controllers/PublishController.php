a23b9d10806eab2833f40e7997eade89190b7c5e
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2016-08-04 14:53:15 +0800

    controller tmp

--- app/Http/Controllers/PublishController.php
+++ app/Http/Controllers/PublishController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Repositories\PublishRepository;
+
 class PublishController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+
+    }
 }
