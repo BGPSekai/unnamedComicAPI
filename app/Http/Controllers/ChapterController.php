886658b7677d79cb580007322347a1820b88dbeb
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2016-08-09 17:31:27 +0800

    回傳新增http error code bug-fix 智障變數錯誤

--- app/Http/Controllers/ChapterController.php
+++ app/Http/Controllers/ChapterController.php
@@ -12,25 +12,25 @@ use Response;
 
 class ChapterController extends Controller
 {
-    public function show($img)
+    public function show()
     {
         try {
             if (!JWTAuth::getToken())
-                return response()->json(['msg' => 'error', 'msg' => 'A token is required.']);
+                return response()->json(['msg' => 'error', 'msg' => 'A token is required.'], 400);
 
 	    	$info = JWTAuth::decode(JWTAuth::getToken())->toArray();
         } catch (JWTException $e) {
-            return response()->json(['status' => 'error', 'msg' => 'Invalid credentials.']);
+            return response()->json(['status' => 'error', 'msg' => 'Invalid credentials.'], 500);
         }
 
     	$comic_id = $info['comic_id'];
     	$chapter_id = $info['chapter_id'];
     	$imgs = $info['imgs'];
 
-    	if ($imgs < 1 || $imgs > $img)
-    		return response()->json(['status' => 'error', 'msg' => 'Page does not exist.']);
+    	if ($page < 1 || $page > $imgs)
+    		return response()->json(['status' => 'error', 'msg' => 'Page does not exist.'], 404);
 
     	$image_path = storage::files('comics/'.$comic_id.'/'.$chapter_id);
-        return Response::download(storage_path().'/app/'.$image_path[$img-1]);
+        return Response::download(storage_path().'/app/'.$image_path[$page-1]);
     }
 }
