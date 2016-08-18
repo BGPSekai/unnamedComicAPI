2a6cb01788769135ca89bc4a143181886308017a
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2016-08-18 15:08:31 +0800

    新增type表、type model、db seed 希望我記得我要幹嘛

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 
+use App\Entities\User;
+use App\Entities\Type;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        User::create(
+        	[
+        		'name' => 'test',
+        		'email' => 'test@test.com',
+        		'password' => Hash::make('secret')
+        	]
+        );
+
+        $types = ['已完結', '少女', '百合', '日常', '玄幻', '遊戲', '治癒', '科幻', '搞笑', '鬼怪', '勵志', '運動', '格鬥', '熱血', '愛情', '校園', '耽美', '偽娘', '冒險', '職場', '後宮', '親情', '戰爭', '懸疑', '偵探', '奇幻', '魔法', '恐怖', '萌系', '歷史', '美食', '競技', '機戰'];
+        foreach ($types as $type) {
+	        Type::create(
+	        	[
+	        		'name' => $type,
+	        		'create_by' => 0
+	        	]
+	        );
+        }
     }
 }
