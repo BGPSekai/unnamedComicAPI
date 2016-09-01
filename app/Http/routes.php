315a39383ea4d80b7178788806fbd49df75d0205
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2016-09-01 15:18:45 +0800

    無意義修改= =

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
 	return view('welcome');
 });
 
+Route::get('/', function () {
+	return view('welcome');
+});
+
 Route::group(['prefix' => 'api', 'middleware' => 'cors'], function()
 {
 	Route::group(['prefix' => 'auth'], function()
