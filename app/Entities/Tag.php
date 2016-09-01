8bb1cbb6d3f53d53017dc7b738ae0b5cae370919
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2016-09-01 20:02:40 +0800

    Tag Entity修正

--- app/Entities/Tag.php
+++ app/Entities/Tag.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     protected $fillable = [
-        'comic_id', 'tag', 'tag_by',
+        'comic_id', 'name', 'tag_by',
     ];
 }
