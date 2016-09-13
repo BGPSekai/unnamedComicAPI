2a9d8991c64805f4f91c77e6caa1ee3974fc9493
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2016-09-13 19:43:31 +0800

    user avatar tmp

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,6 +26,40 @@ class UserController extends Controller
         if (! $user = $this->repo->showDetail($id))
             return response()->json(['status' => 'error', 'message' => 'User Not Found'], 404);
 
-    	return response()->json(['status' => 'success', 'user' => $user]);
+        return response()->json(['status' => 'success', 'user' => $user]);
+    }
+
+    public function avatar(Request $request)
+    {
+        $user = Auth::user();
+        $data = $request->only('image');
+        $validator = $this->validator($data);
+
+        if ($validator->fails())
+            return response()->json(['status' => 'error', 'message' => $validator->errors()->all()], 400);
+ 
+        //User::has_avatar
+
+        $avatar = $request->file('image');
+        $extension = $avatar->getClientOriginalExtension();
+        $this->storeFile('users/'.$user->id.$extension, $avatar);
+
+        return response()->json(['status' => 'success');
+    }
+
+    public function showAvatar($id)
+    {
+        if (! $user = $this->repo->showDetail($id))
+            return response()->json(['status' => 'error', 'message' => 'User Not Found'], 404);
+
+        // $file_path = Storage::files('comics/'.$comic->id);
+        // return Response::download(storage_path().'/app/'.$file_path[0]);
+    }
+
+    private function validator(array $data)
+    {
+        return Validator::make($data, [
+            'iamge' => 'required|image',
+        ]);
     }
 }
