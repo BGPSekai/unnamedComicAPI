8d6939eccb9ef6163ba9cb3a6b66116dfe27895f
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2016-10-18 00:41:53 +0800

    新增收藏

---
+++ app/Repositories/FavoriteRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Entities\Favorite;
+
+class FavoriteRepository
+{
+	public function store(array $data)
+	{
+		return 
+	        Chapter::create([
+	        	'uid' => $data['uid'];
+	        	'comic_id' => $data['comic_id'],
+	        ]);
+	}
+
+	public function destroy($uid, $comic_id)
+	{
+		$favorite = Favorite::where('uid', $uid)
+			->where('comic_id', $comic_id)
+			->first()
+			->id;
+		return Favorite::destroy($favorite);
+	}
+
+	public function showUsers($comic_id)
+	{
+		return Tag::where('comic_id', $comic_id)->pluck('uid');
+	}
+
+	public function showComics($uid)
+	{
+		return Favorite::where('uid', $uid)->pluck('comic_id');
+	}
+}
