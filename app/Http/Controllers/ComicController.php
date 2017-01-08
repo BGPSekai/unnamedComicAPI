1b48fa3aa6fb8c7b2b70df6136c99f5e18f400fe
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2017-01-08 14:48:07 +0800

    砍page jwt

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -8,8 +8,8 @@ use App\Repositories\ComicRepository;
 use App\Repositories\ChapterRepository;
 use Response;
 use Storage;
-use JWTAuth;
-use JWTFactory;
+// use JWTAuth;
+// use JWTFactory;
 use Validator;
 
 class ComicController extends Controller
@@ -35,15 +35,15 @@ class ComicController extends Controller
         }
 
         $chapters = $this->chapterRepo->find($id);
-        foreach ($chapters as $chapter) {
-            $chapter['token'] = (string) JWTAuth::encode(
-                JWTFactory::make([
-                    'comic_id' => $id,
-                    'chapter_id' => $chapter->id,
-                    'pages' => $chapter->pages
-                ])
-            );
-        }
+        // foreach ($chapters as $chapter) {
+        //     $chapter['token'] = (string) JWTAuth::encode(
+        //         JWTFactory::make([
+        //             'comic_id' => $id,
+        //             'chapter_id' => $chapter->id,
+        //             'pages' => $chapter->pages
+        //         ])
+        //     );
+        // }
 
         return response()->json(['status' => 'success', 'comic' => $comic, 'chapters' => $chapters]);
     }
@@ -61,24 +61,29 @@ class ComicController extends Controller
         return Response::download(storage_path().'/app/'.$file_path[0]);
     }
 
-    public function showPage($page)
+    // public function showPage($page)
+    public function showPage($chapter_id, $page)
     {
-        try {
-            if (!JWTAuth::getToken())
-                return response()->json(['status' => 'error', 'message' => 'A Token Is Required'], 400);
+        // try {
+        //     if (!JWTAuth::getToken())
+        //         return response()->json(['status' => 'error', 'message' => 'A Token Is Required'], 400);
 
-            $info = JWTAuth::decode(JWTAuth::getToken())->toArray();
-        } catch (\Exception $e) {
-            return response()->json(['status' => 'error', 'message' => $e->getMessage()], 401);
-        }
+        //     $info = JWTAuth::decode(JWTAuth::getToken())->toArray();
+        // } catch (\Exception $e) {
+        //     return response()->json(['status' => 'error', 'message' => $e->getMessage()], 401);
+        // }
 
-        try {
-            $comic_id = $info['comic_id'];
-            $chapter_id = $info['chapter_id'];
-            $pages = $info['pages'];
-        } catch (\Exception $e) {
-            return response()->json(['status' => 'error', 'message' => $e->getMessage()], 401);
-        }
+        // try {
+        //     $comic_id = $info['comic_id'];
+        //     $chapter_id = $info['chapter_id'];
+        //     $pages = $info['pages'];
+        // } catch (\Exception $e) {
+        //     return response()->json(['status' => 'error', 'message' => $e->getMessage()], 401);
+        // }
+
+        $chapter = $this->chapterRepo->show($chapter_id);
+        $comic_id = $chapter->comic_id;
+        $pages = $chapter->pages;
 
         if ($page < 1 || $page > $pages)
             return response()->json(['status' => 'error', 'message' => 'Page Not Found'], 404);
@@ -87,7 +92,7 @@ class ComicController extends Controller
         natsort($file_path);
         $file_path = array_values($file_path);
         header("Access-Control-Allow-Origin: *");
-        return Response::download(storage_path().'/app/'.$file_path[$page-1]);
+        return Response::download(storage_path().'/app/'.$file_path[--$page]);
     }
 
     public function info(Request $request)
