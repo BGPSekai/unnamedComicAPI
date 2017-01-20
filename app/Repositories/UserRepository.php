3ddfb8355f9cf5913e502474e29260a6d74a8c73
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2017-01-20 11:07:05 +0800

    無意義修改

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -8,7 +8,7 @@ class UserRepository
 {
 	public function create(array $data)
 	{
-		return 
+		return
 	        User::create([
 	            'name' => $data['name'],
 	            'email' => $data['email'],
