9110bf6e15c5c28d6f0710f748b21f42203f23e7
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2017-01-22 01:51:29 +0800

    TagTest、UserTest

--- tests/TagTest.php
+++ tests/TagTest.php
@@ -20,10 +20,39 @@ class TagTest extends TestCase
 		$this->post('/api/tag/test/1')
 			->seeJson(['tags' => [['name' => 'test']]]);
 
-		$this->post('/api/tag/test/2')
-			->assertResponseStatus(404);
-
 		$this->post('/api/tag/test/1')
 			->assertResponseStatus(403);
+
+		$this->get('/api/comic/1')
+			->seeJson(['tags' => [['name' => 'test']]]);
+
+		$this->get('/api/tag/te')
+			->seeJson(['tags' => ['test']]);
+
+		$this->get('/api/tag/ab')
+			->seeJson(['tags' => []]);
+
+		$this->post('/api/tag/test/2')
+			->assertResponseStatus(404);
+	}
+
+	public function testUntag()
+	{
+		JWT::clearToken();
+		$this->delete('/api/tag/test/1')
+			->assertResponseStatus(400);
+
+		JWT::createToken();
+		$this->delete('/api/tag/test/1')
+			->assertResponseOk();
+
+		$this->delete('/api/tag/test/1')
+			->assertResponseStatus(404);
+
+		$this->get('/api/comic/1')
+			->seeJson(['tags' => []]);
+
+		$this->get('/api/tag/te')
+			->seeJson(['tags' => []]);
 	}
 }
---
+++ tests/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('tests/JWT.php');
+
+class UserTest extends TestCase
+{
+	public function testUserInfo()
+	{
+		$this->get('/api/user/1')
+			->assertResponseOk();
+
+		$this->get('/api/user/2')
+			->assertResponseStatus(404);
+
+		$this->get('/api/user')
+			->assertResponseStatus(400);
+
+		JWT::createToken();
+		$this->get('/api/user')
+			->assertResponseOk();
+	}
+}
