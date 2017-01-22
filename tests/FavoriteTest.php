a3c33292a7c868ee9e9ee66b1a21de1444d1d145
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2017-01-23 00:48:36 +0800

    FavoriteTest

--- tests/FavoriteTest.php
+++ tests/FavoriteTest.php
@@ -20,7 +20,26 @@ class FavoriteTest extends TestCase
 		$this->post('/api/favorite/2')
 			->assertResponseStatus(404);
 
+		$this->post('/api/favorite/1')
+			->assertResponseStatus(403);
+
 		$this->get('/api/user/1/favorites')
 			->seeJson(['favorites' => [1]]);
+	}
+
+	public function testDeleteFavorite()
+	{
+		$this->delete('/api/favorite/1')
+			->assertResponseStatus(400);
+
+		JWT::createToken();
+		$this->delete('/api/favorite/1')
+			->seeJson(['favorites' => []]);
+
+		$this->delete('/api/favorite/1')
+			->assertResponseStatus(404);
+
+		$this->get('/api/user/1/favorites')
+			->seeJson(['favorites' => []]);
 	}
 }
