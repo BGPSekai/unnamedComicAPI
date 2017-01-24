c708a369b36e4c135c556bbdea05dc6b4e50f5ff
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2017-01-24 10:34:15 +0800

    修改FavoriteController@store param驗證

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -4,28 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Repositories\ComicRepository;
 use App\Repositories\FavoriteRepository;
 use Auth;
+use Validator;
 
 class FavoriteController extends Controller
 {
-    public function __construct(ComicRepository $comicRepo, FavoriteRepository $favoriteRepo)
+    public function __construct(FavoriteRepository $repo)
     {
-        $this->comicRepo = $comicRepo;
-        $this->favoriteRepo = $favoriteRepo;
+        $this->repo = $repo;
     }
 
     public function store($comic_id)
     {
-        if (! $comic = $this->comicRepo->show($comic_id))
-            return response()->json(['status' => 'error', 'message' => 'Comic Not Found'], 404);
+        $validator = $this->validator(['comic_id' => $comic_id]);
+
+        if ($validator->fails())
+            return response()->json(['status' => 'error', 'message' => $validator->errors()->all()], 400);
 
         $uid = Auth::user()->id;
 
         $data = ['comic_id' => $comic_id, 'uid' => $uid];
 
-        if (! $favorite = $this->favoriteRepo->store($data))
+        if (! $favorite = $this->repo->store($data))
             return response()->json(['status' => 'error', 'message' => 'Favorite Exist'], 403);
 
         return response()->json(['status' => 'success', 'message' => 'Add Favorite Success']);
@@ -36,7 +37,7 @@ class FavoriteController extends Controller
         $uid = Auth::user()->id;
         
         try {
-            $this->favoriteRepo->destroy($uid, $comic_id);
+            $this->repo->destroy($uid, $comic_id);
         } catch (\Exception $e) {
             return response()->json(['status' => 'error', 'message' => 'Favorite Not Found'], 404);
         }
@@ -46,9 +47,16 @@ class FavoriteController extends Controller
 
     public function show($uid)
     {
-        if (! $favorites = $this->favoriteRepo->show($uid))
+        if (! $favorites = $this->repo->show($uid))
             return response()->json(['status' => 'error', 'message' => 'User Not Found'], 404);
 
         return response()->json(['status' => 'success', 'favorites' => $favorites]);
+    }
+
+    private function validator(array $data)
+    {
+        return Validator::make($data, [
+            'comic_id' => 'required|exists:comics,id',
+        ]);
     }
 }
