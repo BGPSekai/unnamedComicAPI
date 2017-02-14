e91310fb720a1b0e312cc913142c29e6ca3b02f0
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2017-02-14 16:11:02 +0800

    更新漫畫資訊

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -8,6 +8,8 @@ use App\Repositories\ComicRepository;
 use App\Repositories\ChapterRepository;
 use Response;
 use Storage;
+use Validator;
+use File;
 
 class ComicController extends Controller
 {
@@ -55,5 +57,39 @@ class ComicController extends Controller
         $file_path = array_values($file_path);
         header("Access-Control-Allow-Origin: *");
         return Response::download(storage_path().'/app/'.$file_path[--$page]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->intersect('name', 'summary', 'author', 'types', 'cover');
+        $data['id'] = $id;
+        $validator = $this->validator($data);
+
+        if ($validator->fails())
+            return response()->json(['status' => 'error', 'message' => $validator->errors()->all()], 400);
+
+        $cover = $request->file('cover');
+        $extension = explode('/', File::mimeType($cover))[1];
+        Storage::put(
+            'comics/'.$id.'/cover.'.$extension,
+            file_get_contents($cover->getRealPath())
+        );
+
+        unset($data['id']);
+        $comic = $this->comicRepo->update($data, $id);
+        return response()->json(['status' => 'success', 'comic' => $comic]);
+    }
+
+    private function validator(array $data)
+    {
+        return Validator::make($data, [
+            'id' => 'exists:comics',
+            'name' => 'max:255',
+            'summary' => 'max:255',
+            'author' => 'max:255',
+            'types' => 'Array',
+            'types.*' => 'exists:types,id',
+            'cover' => 'image',
+        ]);
     }
 }
